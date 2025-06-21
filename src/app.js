93dc72ec7fd3f12ca91db46ccfcb12f6049e5c30
Anthony González <asgtoribio@gmail.com>
2025-06-21 19:29:26 +0000

    modificacion js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function random(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function getdomain(){
+    for(let i = 0; i < pronoun.length; i++){
+      for(let z = 0; z < adj.length; z++){
+        for(let x = 0; x < noun.length; x++){
+          let dominio = pronoun[i] + adj[z] + noun[x] + '.com'+"<br>";
+          document.getElementById("domain").innerHTML += dominio; //se van a acumular a forma de impresion y se hace un salto de espacio por cada uno con el br
+        }
+      }
+    }
+  }
+
+  getdomain();
 };
